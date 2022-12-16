96b137ef27eca07d7c07b995800e63da5f5612ca
Viyas <viyas@tealvue.com>
2022-12-16 18:38:40 +0530

    add : fetch color function

--- pup/Strategy/FindId.js
+++ pup/Strategy/FindId.js
@@ -1,10 +1,3 @@
-// ? blue
-const blueColor = 'rgb(15,194,192)';
-// ? red
-const redColor = 'rgb(253, 60, 82)';
-// ? white
-const whiteClor = 'rgb(255, 255, 255)';
-
 // console.log([blueColor, redColor, whiteClor]);
 // ? blue color
 const bluColor = await arg.$eval('#atm-strike-index-ltp-CE-BUY', n =>
@@ -26,3 +19,16 @@ const whiteColor = await arg.$eval('#atm-strike-index-ltp-PE-BUY', n =>
 );
 
 console.log('        👍 white Color :', JSON.stringify(whiteColor));
+
+// ? function for Color Fetch
+// ? then click opposite site of button
+
+const fetch_Color = async function (c1, c2, c3, c4, ag, id, label) {
+  // ? set the default color values
+  // ? blue
+  const blueColor = 'rgb(15,194,192)';
+  // ? red
+  const redColor = 'rgb(253, 60, 82)';
+  // ? white
+  const whiteClor = 'rgb(255, 255, 255)';
+};
